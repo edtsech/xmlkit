9bf249746ce95235e5d0c9d27dfcce08bc772ff1
Edward Tsech <edtsech@gmail.com>
2013-03-04 15:07:38 +0100

    Update to clojure 1.5

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Few helper functions for parsing XML files."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/data.xml "0.0.7"]])
